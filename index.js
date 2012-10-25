c9b04fcdbed78a8b165aaa804d191c036df61f37
Brian Takita <brian.takita@gmail.com>
2012-10-24 20:11:42 -0700

    Running directory if index.js the file.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 module.exports.exec = function(filename) {
   if (process.argv[1] == filename) {
-    require("kexec")("jasmine-node " + filename);
+    var path = require("path");
+    if (path.basename(filename) == "index.js") {
+      require("kexec")("jasmine-node " + path.dirname(filename));
+    } else {
+      require("kexec")("jasmine-node " + filename);
+    }
   }
 };
