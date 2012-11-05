f7f59ce0352a9bff70e45d88e7029f3195baee5b
Brian Takita <brian.takita@gmail.com>
2012-11-05 15:38:39 -0800

    Added --forceexit flag to instantly kill process.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ module.exports.exec = function(filename) {
   if (process.argv[1] == filename) {
     var path = require("path");
     if (path.basename(filename) == "index.js") {
-      require("kexec")("jasmine-node " + path.dirname(filename));
+      require("kexec")("jasmine-node --forceexit " + path.dirname(filename));
     } else {
-      require("kexec")("jasmine-node " + filename);
+      require("kexec")("jasmine-node --forceexit " + filename);
     }
   }
 };
